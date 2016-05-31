d2a0d0bb892d79ce4dc5c9f0621ffd1c2d997a7b
Mohammad Aghagolzadeh <mgolzade@gmail.com>
2016-05-31 14:59:02 -0700

    added stereo_sla for train/test

--- Examples/Stereo/stereo_kitti.cc
+++ Examples/Stereo/stereo_kitti.cc
@@ -19,13 +19,13 @@
 */
 
 
-#include<iostream>
-#include<algorithm>
-#include<fstream>
-#include<iomanip>
-#include<chrono>
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+#include <iomanip>
+#include <chrono>
 
-#include<opencv2/core/core.hpp>
+#include <opencv2/core/core.hpp>
 
 #include<System.h>
 
