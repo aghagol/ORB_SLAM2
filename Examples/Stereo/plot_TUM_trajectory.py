2d5155330313d8e50a49e40e863a345b50e5f023
Mohammad Aghagolzadeh <mgolzade@gmail.com>
2016-09-19 09:44:07 -0700

    brought some files from the debug branch

---
+++ Examples/Stereo/plot_TUM_trajectory.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+log = 'pioneer_loc'
+
+# filename = 'logs/'+log+'/CameraTrajectory_train_test_2016_03_04.txt'
+
+filename = 'CameraTrajectory_TUMformat.txt'
+# n1 = 3530
+# n2 = 3333
+
+n1 = 5664
+n2 = 4826
+
+# n1 = 9760
+# n2 = 4826
+
+x = np.zeros(n1+n2)
+y = np.zeros(n1+n2)
+z = np.zeros(n1+n2)
+with open(filename) as f:
+	for i, s in enumerate(f):
+		x[i] = s.split()[1]
+		y[i] = s.split()[2]
+		z[i] = s.split()[3]
+
+plt.plot(x[:n1],z[:n1],'bs',linewidth=5)
+plt.plot(x[n1:],z[n1:],'r.',linewidth=2)
+plt.xlim([-100, 50])
+plt.ylim([-50, 100])
+plt.savefig(filename[:-4]+'.png')
+plt.show()
+
